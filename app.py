4b336d2bebebc0940b216b8bd99d5af195a44be1
Walker Hannan <awahannan@gmail.com>
2017-06-24 15:16:13 -0500

    Add basic 404 template

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from bgg_api import BggApi
+
+from flask import Flask, render_template, make_response
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     api = BggApi()
-    return api.requestBuyList()
+    xml = api.requestBuyList()
+    return make_response(xml, 200, {'Content-Type': 'application/xml'})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html'), 404
+
+if __name__ == '__main__':
+    app.run()
